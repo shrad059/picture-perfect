22232b19b4af1ac36ea9a1be03b8379439709ceb
shraddha <shraddhasingh059@gmail.com>
2024-08-19 09:42:54 -0300

    Added the code for the smile_detection_model

---
+++ model/lenet/nn/conv/minivggnet.py
@@ -0,0 +1,55 @@
+# import the necessary packages
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
+from tensorflow.keras import backend as K
+
+
+
+class MiniVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        '''
+        initialize the model along with the input shape to be
+        "channels last" and the channels dimentions itself
+        '''
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using 'channels first'
+        if K.image_data_format() == 'channels_first':
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+            # first CONV => RELU => CONV => RELU => POOL layer set
+        model.add(Conv2D(32, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(32, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # second CONV => RELU => CONV => RELU => POOL layer set
+        model.add(Conv2D(64, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # first (and only) layer of FC => RELU layer
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+
+        return model
